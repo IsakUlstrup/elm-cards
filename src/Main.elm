0705e3e99f51e6ef853ce70874a512a20910b701
Isak <isakulstrup@gmail.com>
2022-05-01 11:11:22 +0200

    added lines between flower tree nodes

--- src/Main.elm
+++ src/Main.elm
@@ -220,30 +220,27 @@ viewPlantTree tree =
 
                 Node s t1 t2 ->
                     Svg.g []
-                        [ Svg.text_ [ Svg.Attributes.textAnchor "middle" ] [ Svg.text (nodeString s) ]
-
-                        -- , Svg.line
-                        --     [ Svg.Attributes.x1 "0"
-                        --     , Svg.Attributes.y1 "0"
-                        --     , Svg.Attributes.x2 "-10"
-                        --     , Svg.Attributes.y2 (String.fromFloat (height + s.length * -1))
-                        --     , Svg.Attributes.stroke "green"
-                        --     , Svg.Attributes.strokeWidth (String.fromFloat s.thickness)
-                        --     ]
-                        --     []
-                        -- , Svg.line
-                        --     [ Svg.Attributes.x1 "0"
-                        --     , Svg.Attributes.y1 "0"
-                        --     , Svg.Attributes.x2 "10"
-                        --     , Svg.Attributes.y2 (String.fromFloat (height + s.length * -1))
-                        --     , Svg.Attributes.stroke "green"
-                        --     , Svg.Attributes.strokeWidth (String.fromFloat s.thickness)
-                        --     ]
-                        --     []
+                        [ Svg.line
+                            [ Svg.Attributes.x1 "0"
+                            , Svg.Attributes.y1 "0"
+                            , Svg.Attributes.x2 "-10"
+                            , Svg.Attributes.y2 (String.fromFloat (20 * -1))
+                            , Svg.Attributes.stroke "green"
+                            , Svg.Attributes.strokeWidth "1"
+                            ]
+                            []
+                        , Svg.line
+                            [ Svg.Attributes.x1 "0"
+                            , Svg.Attributes.y1 "0"
+                            , Svg.Attributes.x2 "10"
+                            , Svg.Attributes.y2 (String.fromFloat (20 * -1))
+                            , Svg.Attributes.stroke "green"
+                            , Svg.Attributes.strokeWidth "1"
+                            ]
+                            []
+                        , Svg.text_ [ Svg.Attributes.textAnchor "middle" ] [ Svg.text (nodeString s) ]
                         , Svg.g [ Svg.Attributes.transform ("translate(-10, " ++ String.fromFloat (20 * -1) ++ ")") ] [ renderNode t1 height ]
                         , Svg.g [ Svg.Attributes.transform ("translate(10, " ++ String.fromFloat (20 * -1) ++ ")") ] [ renderNode t2 height ]
-
-                        -- , Svg.g [ Svg.Attributes.transform "translate(-36 45.5)" ] [ renderNode t2 height ]
                         ]
     in
     svg [ Svg.Attributes.viewBox "-50 -100 100 100" ] [ renderNode tree 0 ]
